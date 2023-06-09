c0f1117d83d193baebd5ae1028ddca8f253a3f2b
Jerome Massot <jerome.massot.78@gmail.com>
2023-06-09 11:40:36 -0700

    add the index search and anwser search ui

--- pages/3_Automatic_Enrichment_of_Search.py
+++ pages/3_Automatic_Enrichment_of_Search.py
@@ -1,4 +1,8 @@
-from utils import list_embedding_palm_models, create_and_fill_glossary, init_sparse_encoder
+from utils import (
+    list_embedding_palm_models, list_generate_palm_models, 
+    create_and_fill_glossary, init_sparse_encoder, search_index,
+    answer
+)
 import streamlit as st
 import pandas as pd
 import pinecone
@@ -32,16 +36,19 @@ def load_glossary(uploaded_file):
 
 
 @st.cache_resource
-def list_embedding_models():
+def list_palm_models():
     """
-    List the embedding models available in the local folder
+    List the embedding models and generative models available in PaLM API
     :return: the list of the embedding models
     """
-    return list_embedding_palm_models()
+    return list_embedding_palm_models(), list_generate_palm_models()
 
 
 # init the pinecone connection    
 init_pinecone()
+
+# list PaLM models
+(embedding_models, embedding_dimensions), generate_models = list_palm_models()
 
 # Title
 st.title('Automatic Semantic enrichment of technical glossary with PaLM')
@@ -70,7 +77,7 @@ st.write("""
     - **Step 2**: encode the glossary with PaLM embedding model
     - **Step 3**: index the glossary with Pinecone
     - **Step 4**: retrieve relevant contexts from the glossary with Pinecone depending on the user query
-    - **Step 5**: generate an answer with PaLM from these ranked contexts
+    - **Step 5**: generate an answer with PaLM generative model from these ranked contexts
     """
 )
 
@@ -81,10 +88,9 @@ uploaded_file = st.sidebar.file_uploader("Choose an csv file", type=['csv'])
 
 st.info("""
     You can upload your own technical glossary by using the uploader in the sidebar menu.
-    The csv file could have 3 columns to be used for ranking the contexts
+    The csv file should contains at least 2 columns to be used for ranking the contexts
     - **column 1**: the term defined in the glossary
-    - **column 2**: a short definition which can be encoded as sparse vectors
-    - **column 3**: the full definition which will be enconded as dense vectors
+    - **column 2**: the full definition which will be enconded as dense vectors
 """)
 
 glossary = None
@@ -109,13 +115,12 @@ st.write("""
 st.info("Use the embedding model selector in the leff sidebar menu, and then click on the button to encode the glossary.")
 
 st.sidebar.subheader("Glossary Sparse Encoding")
-sparse_encode_glossary = st.sidebar.button("Init the sparse encoder from glossry")
+sparse_encode_glossary = st.sidebar.button("Init the sparse encoder from glossary")
 if glossary is not None and len(short_column) > 0 and sparse_encode_glossary:
     bm25_encoder = init_bm25_encoder(glossary, short_column)
     st.session_state['bm25_encoder'] = bm25_encoder
 
 st.sidebar.subheader("Glossary Dense Encoding")
-embedding_models, embedding_dimensions = list_embedding_palm_models()
 embedding_model_name = st.sidebar.selectbox("Embedding model", options=embedding_models)
 
 if embedding_model_name:
@@ -127,14 +132,16 @@ encode_glossary = st.sidebar.button("Encode the glossary")
 if encode_glossary and glossary is not None:
     bm25_encoder = st.session_state['bm25_encoder']
     with st.spinner("Encoding the glossary..."):
-        new_index_stats = create_and_fill_glossary(
+        message_log = create_and_fill_glossary(
             glossary, 
             embedding_model_name, embedding_dimension, bm25_encoder,
             term_column, short_column, definition_column, keywords_column, 
             batch_size=64
         )
-        st.success("Glossary encoded")
-        st.write(new_index_stats)
+        if message_log == "The index already exists...":
+            st.error(message_log)
+        else:
+            st.success("Glossary encoded")
 
 st.subheader("Search Engine")
 
@@ -142,12 +149,69 @@ st.write("""Ask a question related to the technical terms that you have uploaded
 
 st.info("Use the search bar below to ask a question.")
 
+st.sidebar.subheader("Search Engine")
+
+st.sidebar.write(f"Using index: {pinecone.list_indexes()[0]}")
+
+with st.sidebar.form("Search Engine"):
+    top_k = st.number_input("Top K", min_value=1, max_value=10, value=3, step=1)
+    st.caption("Number of contexts to retrieve from the glossary")
+
+    alpha = st.number_input("Alpha", min_value=0, max_value=100, value=50, step=10)
+    alpha = (100-alpha) / 100
+    st.caption("Weight of the BM25 score in the final ranking (0-100)")
+
+    deduplicate = st.checkbox("Deduplicate", value=True)
+    st.caption("Deduplicate the contexts definition using similarity")
+
+    reader_model = st.selectbox("Reader model", options=generate_models)
+    st.caption("Model used to generate the answer")
+
+    answer_button = st.form_submit_button("Answer")
+
 question = st.text_input("Question", key="question")
 
-    #answer_question(question)
+if answer_button and question and len(question) > 0:
+    ranked_contexts = search_index(
+        question, st.session_state['bm25_encoder'], 
+        embedding_model_name, top_k, alpha
+    )
 
+    matches = ranked_contexts["matches"]
 
+    if deduplicate:
+        unique_texts = []
+        unique_matches = []
+        for match in matches:
+            if match["metadata"]["text"] not in unique_texts:
+                unique_matches.append(match)
+                unique_texts.append(match["metadata"]["text"])
+        matches = list(unique_matches)
 
+    if len(matches) > 0:
+        context = ' '.join([match["metadata"]["text"] for match in matches])
+        st.write("Found anwser:")
+        st.write(
+            answer(context, question, reader_model, nb_words=100)
+        )
 
-
-
+    with st.expander("Related terms and keywords"):
+        st.multiselect(
+            "Related terms", 
+            options=[match["metadata"]["term"] for match in matches],
+            default=sorted(match["metadata"]["term"] for match in matches)
+        )
+        st.multiselect(
+            "Related keywords", 
+            options=(k for match in matches for k in match["metadata"]["keywords"]),
+            default=sorted(k for match in matches for k in match["metadata"]["keywords"])
+        )
+    
+    with st.expander("Ranked contexts"):
+        st.write(f"Top {top_k} ranked contexts:")
+        for match in matches:
+            st.write(match["id"])
+            st.write(match["metadata"]["term"])
+            st.write(match["metadata"]["keywords"])
+            st.write(match["metadata"]["text"])
+            st.write(match["score"])
